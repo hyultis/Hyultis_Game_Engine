284479110bdbf6d8fedfb0308bdcd19fb5ebfb4b
hyultis <hyultis@gmail.com>
2024-11-30 11:51:19 +0100

    saved before delete

--- HGE/src/fronts/agnosticHandle.rs
+++ HGE/src/fronts/agnosticHandle.rs
@@ -1,6 +1,7 @@
 use raw_window_handle::{
 	DisplayHandle, HandleError, HasDisplayHandle, HasWindowHandle, WindowHandle,
 };
+use std::sync::{Arc, Mutex};
 
 pub trait HasWindowDisplayHandle: HasWindowHandle + HasDisplayHandle {}
 
@@ -9,7 +10,7 @@ impl<T: HasWindowHandle + HasDisplayHandle> HasWindowDisplayHandle for T {}
 /// this struct iam to simplify conversion of HasWindowHandle + HasDisplayHandle into one for vulkano (because lifetime)
 pub struct agnosticHandle
 {
-	_inner: Box<dyn HasWindowDisplayHandle>,
+	_inner: Arc<Mutex<Box<dyn HasWindowDisplayHandle>>>,
 }
 
 impl agnosticHandle
@@ -17,19 +18,19 @@ impl agnosticHandle
 	pub fn newFromHandle(handle: impl HasWindowDisplayHandle + 'static) -> Self
 	{
 		Self {
-			_inner: Box::new(handle),
+			_inner: Arc::new(Mutex::new(Box::new(handle))),
 		}
 	}
 }
 
-unsafe impl Send for agnosticHandle {}
-unsafe impl Sync for agnosticHandle {}
+//unsafe impl Send for agnosticHandle {}
+//unsafe impl Sync for agnosticHandle {}
 
 impl HasDisplayHandle for agnosticHandle
 {
 	fn display_handle(&self) -> Result<DisplayHandle, HandleError>
 	{
-		self._inner.display_handle()
+		self._inner.lock().unwrap().display_handle()
 	}
 }
 
@@ -37,6 +38,6 @@ impl HasWindowHandle for agnosticHandle
 {
 	fn window_handle(&self) -> Result<WindowHandle<'_>, HandleError>
 	{
-		self._inner.window_handle()
+		self._inner.lock().unwrap().window_handle()
 	}
 }
